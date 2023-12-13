7f1299dec5fcca6e2aa2a8051813a62ea28d4f48
Itay Segev <itaysegev123@gmail.com>
2023-12-13 14:57:25 +0200

    .

--- connection.js
+++ connection.js
@@ -3,15 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connect = () => {
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-      console.log('connected to Mongo DB');
-    })
-    .catch((err) => {
-      console.log(err.message);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      writeConcern: { w: 'majority' },
     });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 };
 
 export default connect;
